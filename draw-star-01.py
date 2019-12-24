74afff8b9631262fd5b8912aea4ae7d6c9e1d54c
KhalidElGazzar <Khalid.elgazzar@gmail.com>
2019-12-24 13:34:00 +0200

    Added draw-star-01.py to draw a star Modified turtle-sample-02 slightly in order to work with array

---
+++ draw-star-01.py
@@ -0,0 +1,8 @@
+import turtle
+fred = turtle.Turtle()
+
+angle = 180-36
+
+for i in range (0, 5):
+    fred.forward(100)
+    fred.right(angle)
--- turtle-sample-02.py
+++ turtle-sample-02.py
@@ -1,8 +1,11 @@
 import turtle
 
 fred = turtle.Turtle()
-fred.color("yellow")
+fred.color("green")
 
-for i in range(0,4):
+# for i in range(0, 4):  
+
+sides = [1, 2, 3, 4]
+for i in sides:  
     fred.forward(100)
     fred.right(90)
