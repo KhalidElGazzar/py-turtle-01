6fc1b52c4b13adf0b7a120e07946b304b76922b6
KhalidElGazzar <Khalid.elgazzar@gmail.com>
2019-12-24 13:25:46 +0200

    ** For "turtle-sample-01.py":        * Modified it to contain two ways of making a loop (array and range). ** For "turtle-sample-012py":        * Made it contain only method to loop (range)

--- turtle-sample-01.py
+++ turtle-sample-01.py
@@ -1,8 +1,12 @@
 import turtle
 
 fred = turtle.Turtle()
-fred.color("yellow")
+fred.color("green")
 
-for i in range(0,4):
+# sides = [1, 2, 3, 4]
+# for i in sides:
+    
+for i in range(0, 4):    
     fred.forward(100)
     fred.right(90)
+
